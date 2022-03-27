3ee7a14d5e43e6218aecedc9e7e41c457604f11a
Lucas Araujo <lucasaraujo1301@gmail.com>
2022-03-27 20:09:28 -0300

    Configured postgres db.

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,20 @@ MAINTAINER Lucas Araujo
 
 ENV PYTHONUNBUFFERED 1
 
-COPY ./requirements.txt /requirements.txt
-RUN pip install -r /requirements.txt
+ENV VIRTUAL_ENV=/py
+RUN python3 -m venv $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 
-RUN mkdir /app
-WORKDIR /app
+COPY ./requirements.txt /requirements.txt
 COPY ./app /app
 
-RUN adduser -D user
-USER user
+WORKDIR /app
+
+RUN pip install --upgrade pip && \
+    apk add --update --no-cache postgresql-client &&\
+    apk add --update --no-cache --virtual .tmp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev &&\
+    pip install -r /requirements.txt &&\
+    apk del .tmp-build-deps
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- app/app/settings.py
+++ app/app/settings.py
@@ -10,20 +10,29 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-upl#iga6rk#-=4zy9r_3ppc_kx@5t+$dtt4)d_2bh%fpizz(x%'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
@@ -76,8 +85,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS')
     }
 }
 
